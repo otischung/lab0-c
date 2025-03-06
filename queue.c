fa8ff8b06d6f8e435d9b5da4ccbd7914bd339aab
Otis Chung <p76124605@gs.ncku.edu.tw>
2025-03-06 10:07:48 +0800

    Implement q_remove_head and tail
    Utilize `list_first_entry` to get the address of the first node of the
    list, delete the node using list_del, `strncpy` the string, and finally
    return the address of the node.
    Use q_remove_head(head->prev, s) in q_remove_tail.
    
    Change-Id: I5576190dd694ea06109705e9e64e8bac48d6ff92

--- queue.c
+++ queue.c
@@ -64,13 +64,27 @@ bool q_insert_tail(struct list_head *head, char *s)
 /* Remove an element from head of queue */
 element_t *q_remove_head(struct list_head *head, char *sp, size_t bufsize)
 {
-    return NULL;
+    if (!head || list_empty(head))
+        return NULL;
+
+    element_t *elem = list_first_entry(head, element_t, list);
+    list_del(&elem->list);
+
+    if (sp && bufsize) {
+        strncpy(sp, elem->value, bufsize);
+        sp[bufsize - 1] = '\0';
+    }
+
+    return elem;
 }
 
 /* Remove an element from tail of queue */
 element_t *q_remove_tail(struct list_head *head, char *sp, size_t bufsize)
 {
-    return NULL;
+    if (!head || list_empty(head))
+        return NULL;
+
+    return q_remove_head(head->prev->prev, sp, bufsize);
 }
 
 /* Return number of elements in queue */
