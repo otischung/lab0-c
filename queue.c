2e91e6fecd20defc9bbcaa6d63655cb0829e27e1
Otis Chung <p76124605@gs.ncku.edu.tw>
2025-03-06 20:58:40 +0800

    Implement q_swap
    Utilize "unsafe" list_for_each to swap nodes
    
    The `n` means the current pointed node.
    --------------
    Beginning
    1 2 3 4
    n
    
    list_move
    2 1 3 4
      n
    
    Next Iteration
    2 1 3 4
        n
    
    list_move
    2 1 4 3
          n
    --------------
    
    Change-Id: Ic0bbd5e969796598d7b931436cbeed195d46fb60

--- queue.c
+++ queue.c
@@ -147,6 +147,14 @@ bool q_delete_dup(struct list_head *head)
 void q_swap(struct list_head *head)
 {
     // https://leetcode.com/problems/swap-nodes-in-pairs/
+    if (!head)
+        return;
+    struct list_head *node;
+    list_for_each (node, head) {
+        if (node->next == head)
+            break;
+        list_move(node, node->next);
+    }
 }
 
 /* Reverse elements in queue */
